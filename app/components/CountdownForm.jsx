37fe5b28ed809df32f0a68e8f3525e54a86ff17e
Hendrik Schaeidt <he.schaeidt@gmail.com>
2016-12-02 15:49:49 +0100

    add tests to countdownform component

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -3,31 +3,46 @@ import React from 'react';
 export default class CountdownForm extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      seconds: '',
+    };
+
     this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
   onSubmit(e) {
     e.preventDefault();
-    const strSeconds = this.seconds.value;
+    const strSeconds = this.state.seconds;
 
-    if (strSeconds.match(/^[0-9]*$/)) {
-      this.seconds.value = '';
+    if (strSeconds.match(/^[0-9]+$/)) {
+      this.setState({
+        seconds: '',
+      });
       this.props.onSetCountdown(parseInt(strSeconds, 10));
     }
   }
 
+  onChange(e) {
+    this.setState({
+      seconds: e.target.value,
+    });
+  }
+
   render() {
+    const { seconds } = this.state;
+
     return (
-      <div>
-        <form ref={(r) => { this.form = r; }} onSubmit={this.onSubmit} className="countdown-form">
-          <input
-            type="text"
-            ref={(r) => { this.seconds = r; }}
-            placeholder="Enter time in seconds"
-          />
-          <button className="button expanded">Start</button>
-        </form>
-      </div>
+      <form onSubmit={this.onSubmit} className="countdown-form">
+        <input
+          type="text"
+          placeholder="Enter time in seconds"
+          onChange={this.onChange}
+          value={seconds}
+        />
+        <button className="button expanded">Start</button>
+      </form>
     );
   }
 }
--- app/components/__tests__/CountdownForm.test.jsx
+++ app/components/__tests__/CountdownForm.test.jsx
@@ -1,7 +1,25 @@
+import React from 'react';
+import { mount } from 'enzyme';
 import CountdownForm from '../CountdownForm';
 
 describe('<CountdownForm />', () => {
   it('should exist', () => {
     expect(CountdownForm).toBeDefined();
+  });
+
+  it('should call onSetCountdown if valid seconds entered', () => {
+    const spy = jest.fn();
+    const countdownForm = mount(<CountdownForm onSetCountdown={spy} />);
+    countdownForm.find('input').first().simulate('change', { target: { value: '109' } });
+    countdownForm.simulate('submit');
+    expect(spy).toHaveBeenCalledWith(109);
+  });
+
+  it('should not call onSetCountdown if invalid value entered', () => {
+    const spy = jest.fn();
+    const countdownForm = mount(<CountdownForm onSetCountdown={spy} />);
+    countdownForm.find('input').first().simulate('change', { target: { value: '109b' } });
+    countdownForm.simulate('submit');
+    expect(spy).not.toHaveBeenCalled();
   });
 });
