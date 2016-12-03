283bffe31271c619befef62d5a748d417399a3e0
Hendrik Schaeidt <he.schaeidt@gmail.com>
2016-12-03 16:49:47 +0100

    add timer component

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,5 +1,67 @@
 import React from 'react';
+import Clock from './Clock';
+import Controls from './Controls';
+import { STATUS } from './Countdown';
 
-export default () => (
-  <p>Timer.jsx</p>
-);
+export default class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+      status: STATUS.STOPPED,
+    };
+
+    this.handleStatusChange = this.handleStatusChange.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.status !== prevState.status) {
+      if (this.state.status === STATUS.STARTED) {
+        this.startTimer();
+      } else {
+        this.stopTimer();
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    this.stopTimer();
+  }
+
+  startTimer() {
+    this.timer = setInterval(() => {
+      const count = this.state.count + 1;
+      this.setState({ count });
+    }, 1000);
+  }
+
+  stopTimer() {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  }
+
+  handleStatusChange(status) {
+    const state = { status };
+
+    if (status === STATUS.STOPPED) {
+      state.count = 0;
+    }
+
+    this.setState(state);
+  }
+
+  render() {
+    const { count, status } = this.state;
+
+    return (
+      <div>
+        <h1 className="page-title">Timer App</h1>
+        <Clock totalSeconds={count} />
+        <Controls countdownStatus={status} onStatusChange={this.handleStatusChange} />
+      </div>
+    );
+  }
+}
