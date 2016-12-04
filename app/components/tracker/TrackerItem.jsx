fede0b8319601bb13dc191bc5524aba289990d57
Hendrik Schaeidt <he.schaeidt@gmail.com>
2016-12-04 12:25:03 +0100

    add proper time formatting and total count of tracked hours per range

--- app/components/tracker/TrackerItem.jsx
+++ app/components/tracker/TrackerItem.jsx
@@ -1,26 +1,42 @@
 import React, { PropTypes } from 'react';
+import moment from 'moment';
 
-const TrackerItem = ({ date, ranges }) => (
-  <div>
-    {date}
+const TrackerItem = ({ date, ranges }) => {
+  const totalDuration = moment.duration();
+
+  const renderRanges = () => ranges.map((range, index) => {
+    const to = moment(range.to);
+    const from = moment(range.from);
+
+    totalDuration.add(moment.duration(to.diff(from)));
+
+    return (
+      <div key={index}>
+        from {from.format('LT')} to {to.format('LT')}
+      </div>
+    );
+  });
+
+  return (
     <div>
-      {ranges.map((range, index) => (
-        <p key={index}>
-          Ranges:
-          <span>From: {range.from}</span>
-          <span>To: {range.to}</span>
-        </p>
-      ))}
+      {moment(date).format('dddd, MMMM Do YYYY')}
+      <div>
+        Time ranges:
+        {renderRanges()}
+      </div>
+      <div>
+        Total time tracked: {totalDuration.hours()} hours {totalDuration.minutes()} minutes
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export const propTypes = {
   date: PropTypes.string.isRequired,
   ranges: PropTypes.arrayOf(
     PropTypes.shape({
-      from: PropTypes.number.isRequired,
-      to: PropTypes.number.isRequired,
+      from: PropTypes.string.isRequired,
+      to: PropTypes.string.isRequired,
     }),
   ).isRequired,
 };
